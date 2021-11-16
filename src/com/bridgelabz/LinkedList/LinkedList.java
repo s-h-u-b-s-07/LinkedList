be695b506236f630e5cbfc150f0efef33b699307
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-16 17:14:31 +0530

    ADDED: Welcome to LinkedList program

---
+++ src/com/bridgelabz/LinkedList/LinkedList.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.LinkedList;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO LINKEDLIST PROGRAM");
+    }
+}
